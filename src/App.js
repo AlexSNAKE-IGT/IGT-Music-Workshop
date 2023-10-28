3880ff3f18d1c0e43c07df5842f9af78b80840d5
Александр Макаров <alexgamechanel2001@gmail.com>
2023-10-28 22:58:16 +0300

    Struct

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import Header from './modules/Header/Header.jsx'
+import Footer from './modules/Footer/Footer.jsx'
+import MusicPage from './pages/MusicPage/MusicPage.jsx'
+
 
 function App() {
   return (
